a9fc896df0844ee0f49c083c1443395ef00e1582
NregnartS <61258087+NregnartS@users.noreply.github.com>
2020-03-13 11:15:30 +0800

    commit homework4 file

---
+++ homework4/Clock/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    class Program
+    {
+        public delegate void ClockHandler();
+        public class Clock
+        {
+            public event ClockHandler Tick;
+            public event ClockHandler Alarm;
+            public DateTime ClockTime{ get; set; }
+            public void ClockEvent(DateTime alarmTime)
+            {
+                DateTime Temp2 = DateTime.Parse(alarmTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                while (true)
+                {
+                    DateTime Temp1 = DateTime.Parse(this.ClockTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                    if (Temp1 == Temp2) Alarm();
+                    else Tick();
+                }
+            }
+        }
+        public class Form
+        {
+            public Clock MyClock = new Clock();
+            public void SetMyClock(DateTime MyClockTime)
+            {
+                MyClock.ClockTime = MyClockTime;
+            }
+            public Form()
+            {
+                MyClock.Tick += Tick;
+                MyClock.Alarm += Alarm;
+            }
+            void Tick()
+            {
+                Console.WriteLine("嘀嗒……");
+                MyClock.ClockTime = MyClock.ClockTime.AddSeconds(1);
+                System.Threading.Thread.Sleep(1000);
+            }
+            void Alarm()
+            {
+                Console.WriteLine("叮零零零零……");
+                MyClock.ClockTime = MyClock.ClockTime.AddSeconds(1);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Form form1 = new Form();
+            Console.WriteLine("请设置时钟初始时间(若输入格式错误则默认为当前系统时间)：");
+            string Time=Console.ReadLine();
+            DateTime InputTime;
+            if (DateTime.TryParse(Time, out InputTime))
+            {
+                InputTime = DateTime.Parse(Time);
+                form1.SetMyClock(InputTime);
+                Console.WriteLine($"初始时钟时间为：{form1.MyClock.ClockTime}");
+            }
+            else
+            {
+                form1.SetMyClock(DateTime.Now);
+                Console.WriteLine($"初始时钟时间为：{form1.MyClock.ClockTime}");
+            }
+            while (true)
+            {
+                Console.WriteLine("请设置闹钟时间：");
+                string AlarmTime = Console.ReadLine();
+                DateTime InPutAlarmTime;
+                if (DateTime.TryParse(AlarmTime, out InPutAlarmTime))
+                {
+                    InPutAlarmTime = DateTime.Parse(AlarmTime);
+                    form1.MyClock.ClockEvent(InPutAlarmTime);
+                }
+                else
+                {
+                    Console.WriteLine("输入有误，请按任意键重新输入：");
+                    Console.ReadKey();
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ homework4/homework4/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for(Node<T> Item=head;Item!=null;Item=Item.Next)
+            {
+                action(Item.Data);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GenericList<int> MyList = new GenericList<int>();
+            Console.WriteLine("请依次输入10个值作为数组元素：");
+            for(int x=0;x<10;x++)
+            {
+                MyList.Add(int.Parse(Console.ReadLine()));
+            }
+            MyList.ForEach(Item => Console.Write("该数组元素依次为："+Item+" "));
+            int sum = 0,max=int.MinValue,min=int.MaxValue;
+            MyList.ForEach(Item => sum += Item);
+            MyList.ForEach(Item => { if (max < Item) max = Item; });
+            MyList.ForEach(Item => { if (min > Item) min = Item; });
+            Console.WriteLine($"该数组元素和为：{sum}");
+            Console.WriteLine($"该数组最大值为：{max}");
+            Console.WriteLine($"该数组最小值为：{min}");
+            Console.ReadKey();
+        }
+    }
+}
