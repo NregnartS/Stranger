8b7ac0c39904b31d704177e0f4c788d2eb38038a
NregnartS <61258087+NregnartS@users.noreply.github.com>
2020-05-21 12:47:27 +0800

    commit homework8

---
+++ homework8/homework8/Form1.cs
@@ -0,0 +1,154 @@
+﻿using homework5;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+
+namespace homework8
+{
+    public partial class Form1 : Form
+    {
+        OrderService orderService;
+        void SearchOrderComboBox()
+        {
+            SearchComboBox.Items.Add("订单号");
+            SearchComboBox.Items.Add("商品名称");
+            SearchComboBox.Items.Add("客户名称");
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            orderService = new OrderService();
+            Goods goods = new Goods(100, "信纸",1.0);
+            Customer customer = new Customer("李");
+            OrderItem orderItem = new OrderItem(customer, goods);
+            Order order = new Order(001, orderItem);
+            orderService.AddOrder(order);
+            Goods sgoods = new Goods(10, "水杯", 10.0);
+            Customer scustomer = new Customer("王");
+            OrderItem sorderItem = new OrderItem(scustomer, sgoods);
+            Order sorder = new Order(002, sorderItem);
+            orderService.AddOrder(sorder);
+            OrderBindingSource.DataSource = orderService.OrderData;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SearchOrderComboBox();
+        }
+        private void QueryAll()
+        {
+            OrderBindingSource.DataSource = orderService.OrderData;
+            OrderBindingSource.ResetBindings(false);
+        }
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SearchOrder_Click(object sender, EventArgs e)
+        {
+            switch (SearchComboBox.Text)
+            {
+                case "订单号":
+                    string id = Interaction.InputBox("请输入订单号：", "按订单号查询","", -1,-1);
+                    int ID;
+                    if (id == "")
+                    { MessageBox.Show("订单号不能为空！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                    else if (!int.TryParse(id, out ID))
+                    {
+                        MessageBox.Show("订单号只能由数字构成！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        List<Order> orders = orderService.SelectOrderByID(int.Parse(id));
+                        orderService.OrderData = orders;
+                    }
+                    break;
+                case "商品名称":
+                    string good = Interaction.InputBox("请输入商品名称：", "按商品名称查询", "", -1, -1);
+                    if (good == "")
+                    { MessageBox.Show("商品名称不能为空！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                    else
+                    {
+                        List<Order> orders = orderService.SelectOrderByName(good);
+                        orderService.OrderData = orders;
+                    }
+                    break;
+                case "客户名称":
+                    string customer = Interaction.InputBox("请输入客户名称：", "按客户名称查询", "", -1, -1);
+                    if (customer == "")
+                    { MessageBox.Show("客户名称不能为空！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+                    else
+                    {
+                        List<Order> orders = orderService.SelectOrderByCustomer(customer);
+                        orderService.OrderData = orders;
+                    }
+                    break;
+                default:
+                    MessageBox.Show("请选择查询条件！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+            }
+            OrderBindingSource.ResetBindings(true);
+        }
+
+        private void CreateOrder_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(orderService);
+            form2.ShowDialog();
+            orderService = form2.OrderServic;
+            OrderBindingSource.ResetBindings(false);
+        }
+
+        private void DeleteOrder_Click(object sender, EventArgs e)
+        {
+            string id = Interaction.InputBox("请输入订单号：", "删除订单", "", -1, -1);
+            int ID;
+            if (!int.TryParse(id, out ID))
+            {
+                MessageBox.Show("订单号只能由数字构成！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            orderService.DeleteOrder(int.Parse(id));
+            OrderBindingSource.ResetBindings(false);
+        }
+
+        private void ModifyOrder_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2(orderService);
+            form2.ShowDialog();
+            orderService = form2.OrderServic;
+            OrderBindingSource.ResetBindings(false);
+        }
+
+        private void OutOrder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = saveFileDialog1.ShowDialog();
+            if (result.Equals(DialogResult.OK))
+            {
+                String fileName = saveFileDialog1.FileName;
+                orderService.Export(fileName);
+            }
+        }
+
+        private void InOrder_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result.Equals(DialogResult.OK))
+            {
+                String fileName = openFileDialog1.FileName;
+                orderService.Import(fileName);
+                QueryAll();
+            }
+        }
+    }
+}
+    
---
+++ homework8/homework8/Form2.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace homework8
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.FunName = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Add = new System.Windows.Forms.Button();
+            this.Modify = new System.Windows.Forms.Button();
+            this.Back = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.NumTextBox = new System.Windows.Forms.TextBox();
+            this.PriceTextBox = new System.Windows.Forms.TextBox();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.CustomerTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OrderID = new System.Windows.Forms.Label();
+            this.IdTextBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.FunName);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // FunName
+            // 
+            this.FunName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.FunName.AutoSize = true;
+            this.FunName.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FunName.Location = new System.Drawing.Point(18, 37);
+            this.FunName.Name = "FunName";
+            this.FunName.Size = new System.Drawing.Size(166, 24);
+            this.FunName.TabIndex = 0;
+            this.FunName.Text = "添加/删除订单";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.Add);
+            this.flowLayoutPanel1.Controls.Add(this.Modify);
+            this.flowLayoutPanel1.Controls.Add(this.Back);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(240, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(485, 103);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(3, 3);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(155, 97);
+            this.Add.TabIndex = 0;
+            this.Add.Text = "添加订单";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Modify
+            // 
+            this.Modify.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Modify.Location = new System.Drawing.Point(164, 3);
+            this.Modify.Name = "Modify";
+            this.Modify.Size = new System.Drawing.Size(155, 97);
+            this.Modify.TabIndex = 1;
+            this.Modify.Text = "修改订单";
+            this.Modify.UseVisualStyleBackColor = false;
+            this.Modify.Click += new System.EventHandler(this.Modify_Click);
+            // 
+            // Back
+            // 
+            this.Back.Location = new System.Drawing.Point(325, 3);
+            this.Back.Name = "Back";
+            this.Back.Size = new System.Drawing.Size(155, 97);
+            this.Back.TabIndex = 2;
+            this.Back.Text = "返回上一界面";
+            this.Back.UseVisualStyleBackColor = true;
+            this.Back.Click += new System.EventHandler(this.Back_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.NumTextBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.PriceTextBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.NameTextBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.CustomerTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label7, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.OrderID, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.IdTextBox, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 129);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52.89256F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 47.10744F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 62F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 61F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(714, 309);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // NumTextBox
+            // 
+            this.NumTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.NumTextBox.Location = new System.Drawing.Point(417, 265);
+            this.NumTextBox.Name = "NumTextBox";
+            this.NumTextBox.Size = new System.Drawing.Size(236, 28);
+            this.NumTextBox.TabIndex = 14;
+            // 
+            // PriceTextBox
+            // 
+            this.PriceTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.PriceTextBox.Location = new System.Drawing.Point(417, 204);
+            this.PriceTextBox.Name = "PriceTextBox";
+            this.PriceTextBox.Size = new System.Drawing.Size(236, 28);
+            this.PriceTextBox.TabIndex = 13;
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.NameTextBox.Location = new System.Drawing.Point(417, 143);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(236, 28);
+            this.NameTextBox.TabIndex = 12;
+            // 
+            // CustomerTextBox
+            // 
+            this.CustomerTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CustomerTextBox.Location = new System.Drawing.Point(417, 82);
+            this.CustomerTextBox.Name = "CustomerTextBox";
+            this.CustomerTextBox.Size = new System.Drawing.Size(236, 28);
+            this.CustomerTextBox.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(129, 270);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 18);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "商品数量：";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(129, 209);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(98, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "商品单价：";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "商品名称：";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "客户名称：";
+            // 
+            // OrderID
+            // 
+            this.OrderID.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.OrderID.AutoSize = true;
+            this.OrderID.Location = new System.Drawing.Point(138, 24);
+            this.OrderID.Name = "OrderID";
+            this.OrderID.Size = new System.Drawing.Size(80, 18);
+            this.OrderID.TabIndex = 0;
+            this.OrderID.Text = "订单号：";
+            // 
+            // IdTextBox
+            // 
+            this.IdTextBox.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.IdTextBox.Location = new System.Drawing.Point(417, 19);
+            this.IdTextBox.Name = "IdTextBox";
+            this.IdTextBox.Size = new System.Drawing.Size(236, 28);
+            this.IdTextBox.TabIndex = 1;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label FunName;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Modify;
+        private System.Windows.Forms.Button Back;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox NumTextBox;
+        private System.Windows.Forms.TextBox PriceTextBox;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.TextBox CustomerTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label OrderID;
+        private System.Windows.Forms.TextBox IdTextBox;
+    }
+}
